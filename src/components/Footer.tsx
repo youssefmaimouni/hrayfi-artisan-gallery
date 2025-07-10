cd6564258b3c7a6faaac934fb690055149e766e9
youssefmaimouni <135459547+youssefmaimouni@users.noreply.github.com>
2025-07-10 16:01:20 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Separator } from '@/components/ui/separator';
+import  logoJLM  from '@/assets/JLM2.png'; // Adjust the path as necessary  
 
 const Footer = () => {
   const [visitorCount, setVisitorCount] = useState(0);
@@ -21,6 +22,10 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row justify-between items-center space-y-2 md:space-y-0">
           <div className="text-sm text-muted-foreground">
             © {new Date().getFullYear()} JLM ENSA Khouribga. All rights reserved.
+          </div>
+          <div className="flex items-center space-x-2">
+            <img src={logoJLM} alt="JLM Logo" className="h-8 w-auto" />
+            <span className="text-sm text-muted-foreground">Powered by JLMENSAKH 2025</span>
           </div>
           <div className="text-sm text-muted-foreground">
             Visitors: {visitorCount.toLocaleString()}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 import { Search, Heart, ShoppingBag, Menu, Moon, Sun, LogIn, LogOut, User } from 'lucide-react';
-import logoImg from '@/assets/hrayfia-logo.jpg';
+import logoImg from '@/assets/hrayfia-logo-removebg-preview.png';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useTheme } from '@/hooks/useTheme';
